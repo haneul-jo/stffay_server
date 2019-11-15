51b6207b69327254730241caa2bf79ea90488939
Hanseong Park <hs88610@gmail.com>
2019-11-15 16:40:22 +0900

    로그인, 유저정보 세션 라우터 추가

--- routes/authentication/authentication.js
+++ routes/authentication/authentication.js
@@ -8,8 +8,8 @@ router.post("/signin", (req, res) => {
   // 비밀번호 데이터 타입 검사 (문자열인지 아닌지)
   if (typeof req.body.password !== "string") {
     return res.status(401).json({
-      error: "PASSWORD IS NOT STRING",
-      code: 1
+      success: false,
+      errorMsg: "비밀번호가 맞지 않습니다."
     });
   }
 
@@ -19,36 +19,85 @@ router.post("/signin", (req, res) => {
     // 검색 결과가 존재하지 않는 경우
     if (!account) {
       return res.status(401).json({
-        error: "THERE IS NO USER",
-        code: 2
+        success: false,
+        errorMsg: "등록되지 않은 ID입니다."
       });
     }
+
     // 유저검색 결과가 있으면 검사 salt값으로 해쉬
-    const validate = hasher(
-      { password: req.body.password, salt: account.salt },
-      function(err, pass, salt, hash) {
-        // 입력한 비밀번호를 이용해 만는 해쉬와 DB에 저장된 비밀번호가 같을 경우
-        if (hash === account.password) {
-          let session = req.session;
+    // const validate =
+    hasher({ password: req.body.password, salt: account.salt }, function(
+      err,
+      pass,
+      salt,
+      hash
+    ) {
+      // 입력한 비밀번호를 이용해 만든 해쉬와 DB에 저장된 비밀번호가 같을 경우
+      if (hash === account.password) {
+        console.log(" >>> req.session");
+        console.log(req.session);
 
-          session.loginInfo = {
-            _id: account._id,
-            username: account.username
-          };
+        req.session.loginInfo = {
+          _id: account._id,
+          username: account.username
+        };
 
-          // RETURN SUCCESS
-          return res.json({
-            success: true
-          });
-        } else {
-          return res.status(401).json({
-            error: "PASSWORD IS NOT CORRECT",
-            code: 3
-          });
-        }
+        req.session.save(err => {
+          console.log(">> session 저장 >>");
+          console.log(req.session);
+          console.log(">> req check  >>");
+          console.log(req);
+          if (!err) {
+            return res.json({
+              success: true
+            });
+          } else {
+            return res.json({
+              success: false
+            });
+          }
+        });
+      } else {
+        return res.status(401).json({
+          success: false,
+          errorMsg: "비밀번호가 맞지 않습니다."
+        });
       }
-    );
+    });
   });
+});
+
+/*
+    GET CURRENT USER INFO GET /api/authentication/getInfo
+*/
+router.get("/loginInfo", (req, res) => {
+  // console.log(" >>>> req >>> /loginInfo");
+  // console.log(req);
+  console.log(">> 0");
+  console.log(" >>>>>>> req.session >>> /loginInfo");
+  console.log(req.session);
+  // console.log(" >>>>>>> req.session.loginInfo >>> /loginInfo");
+  // console.log(req.session.loginInfo);
+
+  if (typeof req.session.loginInfo === "undefined") {
+    console.log(">> 1");
+    return res.status(401).json({
+      success: false,
+      error: "로그인 정보가 없습니다."
+    });
+  }
+  console.log(">> 2");
+  res.json({ loginInfo: req.session.loginInfo });
+});
+
+/*
+    LOGOUT: POST /api/account/logout
+*/
+router.post("/logout", (req, res) => {
+  req.session.destroy(err => {
+    if (err) throw err;
+  });
+  return res.json({ sucess: true });
 });
 
 module.exports = router;
