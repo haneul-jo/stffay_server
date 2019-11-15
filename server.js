9fedce3ee7fbb90b8ab5290cee3e9a0d44a2e4a8
Hanseong Park <hs88610@gmail.com>
2019-11-15 16:40:50 +0900

    cookieParser, MongoStore 추가

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const logger = require("morgan");
+const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
 const app = express();
 const PORT = 4000;
+const router = require("./routes/index");
 
 // const dbRoute =
 //   "mongodb+srv://admin:admin1234@cluster0-ndqf2.mongodb.net/test?retryWrites=true&w=majority";
@@ -13,26 +16,34 @@ const dbRoute = "mongodb://localhost/staffy";
 mongoose.connect(dbRoute, { useNewUrlParser: true });
 let db = mongoose.connection;
 
-/* use session */
-app.use(
-  session({
-    secret: "staffy1234",
-    resave: false,
-    saveUninitialized: true
-  })
-);
-
 db.once("open", () => console.log("connected to the database >>>>>>> "));
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
-
-const router = require("./routes/index");
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(logger("dev"));
+/* use session */
+app.use(cookieParser("staffy1234"));
+app.use(
+  session({
+    secret: "staffy1234",
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+      url: dbRoute,
+      collection: "sessions"
+    })
+  })
+);
 app.use(router);
+
+/* handle error */
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 app.listen(PORT, function() {
   console.log("Server is running on Port: " + PORT);
