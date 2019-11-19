bae64e89e6fd76bc50814d2405e926eb22caa495
Hanseong Park <hs88610@gmail.com>
2019-11-19 18:31:36 +0900

    옥션 연동 api 추가

--- routes/authentication/authentication.js
+++ routes/authentication/authentication.js
@@ -84,7 +84,7 @@ router.post("/logout", (req, res) => {
   req.session.destroy(err => {
     if (err) throw err;
   });
-  return res.json({ sucess: true });
+  return res.json({ success: false });
 });
 
 module.exports = router;
